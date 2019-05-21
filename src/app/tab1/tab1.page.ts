16e87c9c224c7d943007d932536f996a5e549733
Jesus Jose Saenz Fierro <jaredmaster32@gmail.com>
2019-05-21 02:41:27 -0600

    Push cosillas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ImageService} from '../api/image.service';
 import {Router} from '@angular/router';
-import { File, FileEntry } from '@ionic-native/file';
-import { HttpClient } from '@angular/common/http';
-import { WebView } from '@ionic-native/ionic-webview/ngx';
+import {File, FileEntry} from '@ionic-native/file';
+import {HttpClient} from '@angular/common/http';
+import {WebView} from '@ionic-native/ionic-webview/ngx';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Image} from '../models/Image';
 
@@ -19,18 +19,18 @@ export class Tab1Page implements OnInit {
 
 
   constructor(
-      public imageService: ImageService, private router: Router,
-      private http: HttpClient,
-      private fb: FormBuilder
+    public imageService: ImageService, private router: Router,
+    private http: HttpClient,
+    private fb: FormBuilder
   ) {
-      this.createForm();
+    this.createForm();
   }
 
   createForm() {
-      this.form = this.fb.group({
-          title: null,
-          uploadImage: null
-      });
+    this.form = this.fb.group({
+      title: null,
+      uploadImage: null
+    });
   }
 
   ngOnInit(): void {
@@ -38,43 +38,48 @@ export class Tab1Page implements OnInit {
 
   ionViewWillEnter() {
     this.imageService.getImages().subscribe(
-        (data: any) => {this.images = data; console.log(this.images); },
-        (error) => {console.log(error); }
-        );
+      (data: any) => {
+        this.images = data;
+        console.log(this.images);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   uploadImage() {
-      this.upload.id = 0;
-      this.upload.user = 1;
-      console.log(this.upload);
+    this.upload.id = 0;
+    this.upload.user = 1;
+    console.log(this.upload);
   }
 
   onFileChange(event) {
     const reader = new FileReader();
     if (event.target.files && event.target.files.length > 0) {
-        const file = event.target.files[0];
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-            if (typeof reader.result === 'string') {
-                this.form.get('uploadImage').setValue({
-                    filename: file.name,
-                    filetype: file.type,
-                    value: reader.result.split(',')[1]
-                });
-            }
-        };
+      const file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          this.form.get('uploadImage').setValue({
+            filename: file.name,
+            filetype: file.type,
+            value: reader.result.split(',')[1]
+          });
+        }
+      };
     }
   }
 
   onSubmit() {
-      this.upload.title = this.form.value.title;
-      this.upload.image =  'data:image/jpeg;base64,' + this.form.value.uploadImage.value;
-      this.upload.id = 1;
-      this.upload.user = 1;
-      console.log(this.upload);
-      this.imageService.postImage(this.upload).subscribe(() => {
-         this.router.navigate(['/']);
-      });
+    this.upload.title = this.form.value.title;
+    this.upload.image = 'data:image/jpeg;base64,' + this.form.value.uploadImage.value;
+    this.upload.id = 1;
+    this.upload.user = 1;
+    console.log(this.upload);
+    this.imageService.postImage(this.upload).subscribe(() => {
+      this.router.navigate(['/']);
+    });
   }
 }
 
